ed4731244f573a736a20bcb1831d29d45727fc0f
Vincent Healy <vchealy@gmail.com>
2023-03-03 08:48:13 +0000

    Temperature Convertor
    Celsuis to Fahrenheit
    Fahrenheit to Celsius

---
+++ day6b.temperature_convertor.py
@@ -0,0 +1,18 @@
+# celsuis_fahrenheit
+
+'''
+Convert Celsius to Fahrenheit.
+- `F = ((9/5) * C) + 32`
+Convert Fahrenhiet to Celsius
+- `C = (F-32)*(5/9)`
+'''
+def celsius_to_fahrenheit(temp):
+  return (((9/5) * temp) + 32)
+
+
+def fahrenheit_to_celsius(temp):
+  return ((temp - 32) * (5/9))
+
+
+print(celsius_to_fahrenheit(30))
+print(fahrenheit_to_celsius(86))
